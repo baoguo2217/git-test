a07e80055a896c6fbab29dfaa4af19fbb0b2e6ee
bobYan <baoguo2217@gmail.com>
2022-07-26 23:07:57 +0800

    first commit

---
+++ src/main/java/com/bob/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bob.git;
+
+/**
+ * @author bob
+ * @create 2022/7/26 23:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
